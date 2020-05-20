1c89e5d34919512f925393de5c48631769e40331
James Miller <james.rex.miller.4@gmail.com>
2020-05-20 09:33:53 -0600

    Refactor index.js, create separate schema.graphql file put typeDefs in there

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
 
 let links = [{
   id: 'link-0',
@@ -32,7 +20,7 @@ const resolvers = {
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './schema.graphql',
   resolvers,
 });
 server.start(() => console.log('Server is running on http://localhost:4000'));
