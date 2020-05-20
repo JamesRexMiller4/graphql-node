76179e233ab98c332064aa18b80f8e3195bce972
James Miller <james.rex.miller.4@gmail.com>
2020-05-20 15:36:55 -0600

    Add subscribe logic for Vote

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,5 +1,5 @@
 function newLinkSubscribe(parent, args, context, info) {
-  return context.prisma.$subscribe.link({ mutation_in: ['CREATED'] }).node()
+  return context.prisma.$subscribe.link({ mutation_in: ['CREATED'] }).node();
 }
 
 const newLink = {
@@ -9,6 +9,18 @@ const newLink = {
   },
 }
 
+function newVoteSubscribe(parent, args, context, info) {
+  return context.prisma.$subscribe.vote({ mutation_in: ['CREATED'] }).node();
+}
+
+const newVote = {
+  subscribe: newVoteSubscribe,
+  resolve: payload => {
+    return payload
+  },
+};
+
 module.exports = {
   newLink,
+  newVote
 }
